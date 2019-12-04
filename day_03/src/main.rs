9bc983f75cfa780e5f1e4efb6579e112ab012c33
Adam Harvie <adam.harvie@gmail.com>
2019-12-04 10:11:13 -0500

    WIP day03 again

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -4,6 +4,11 @@ use std::fs::File;
 fn main() {
     let tests = [
         [
+            vec!["R8","U5","L5","D3"],
+            vec!["U7","R6","D4","L4"],
+            vec!["6"]
+        ],
+        [
             vec!["R75","D30","R83","U83","L12","D49","R71","U7","L72"],
             vec!["U62","R66","U55","R34","D71","R55","D58","R83"],
             vec!["159"],
@@ -20,25 +25,28 @@ fn main() {
         println!("Actual:\n {:?}\n", closest_intersection(&test[0], &test[1]));
     }
 
-    // let input = get_input();
-    // println!("Final answer part 1: {}", intcode_compute_input(&input, 12, 2)[0]);
+    let input = get_input();
+    println!("Final answer part 1: {}", closest_intersection(&input[0], &input[1]));
     
 }
 
-// fn get_input() -> Vec<i32> {
-//     let f = File::open("input/input.txt").unwrap();
-//     let reader = BufReader::new(f);
-//     let mut values = Vec::new();
+fn get_input() -> [Vec<&'static str>; 2] {
+    let f = File::open("input/input.txt").unwrap();
+    let reader = BufReader::new(f);
+    let mut values1 = Vec::new();
+    let mut values2 = Vec::new();
     
-//     for line in reader.lines() {
-//         for symbol in line.unwrap().split(",") {
-//             let input: i32 = symbol.parse().unwrap();
-//             values.push(input);
-//         }
-//     }
-
-//     return values;
-// }
+    for (index,line) in reader.lines().enumerate() {
+        if index == 0 {
+            values1 = line.unwrap().split(",").collect();
+        }
+        else {
+            values2 = line.unwrap().split(",").collect();
+        }
+    }
+    println!("{:?}",values1);
+    return [values1, values2];
+}
 
 fn closest_intersection(wire_1: &Vec<&str>, wire_2: &Vec<&str>) -> i32 {
     let path_1 = calculate_path(wire_1);
@@ -48,23 +56,38 @@ fn closest_intersection(wire_1: &Vec<&str>, wire_2: &Vec<&str>) -> i32 {
 }
 
 fn calculate_path(wire: &Vec<&str>) -> Vec<[i32;2]> {
-    let path = Vec::new();
+    let mut path = Vec::new();
+    let mut x = 0;
+    let mut y = 0;
+    path.push([x,y]);
     for step in wire.iter() {
         let direction = step.chars().nth(0).unwrap();
         let distance: i32 = step[1..].parse().unwrap();
-        println!("{:?}", distance);
+        
         match direction {
             'U' => {
-                println!("Up!");
+                for yy in (y+1)..=(y+distance) {
+                    y = yy;    
+                    path.push([x,y]);
+                }
             },
             'D' => {
-                println!("Down!");
+                for yy in ((y-distance)..y).rev() {
+                    y = yy;
+                    path.push([x,y]);
+                }
             },
             'L' => {
-                println!("Left!");
+                for xx in ((x-distance)..x).rev() {
+                    x = xx;
+                    path.push([x,y]);
+                }
             },
             'R' => {
-                println!("Right!");
+                for xx in (x+1)..=(x+distance) {
+                    x = xx;
+                    path.push([x,y]);
+                }
             },
             _ => {
                 panic!(":c");
@@ -72,57 +95,41 @@ fn calculate_path(wire: &Vec<&str>) -> Vec<[i32;2]> {
 
         }
     }
+    println!("{:?}",path);
     return path;
 }
 
 fn get_intersections(path_1: Vec<[i32;2]>, path_2: Vec<[i32;2]>) -> Vec<[i32;2]> {
     let mut intersections = Vec::new();
+    for point in path_1.iter() {
+        if path_2.contains(point) {
+            intersections.push(point.clone());
+        }
+    }
+    for point in path_2.iter() {
+        if (path_1.contains(point) && !intersections.contains(point)) {
+            intersections.push(point.clone());
+        }
+    }
+    println!("{:?}",intersections);
     return intersections;
 }
 
 fn get_min_distance(intersections: Vec<[i32;2]>) -> i32 {
-    let min = std::i32::MAX;
+    let mut min = std::i32::MAX;
+    for point in intersections.iter() {
+        if point[0] == 0 && point[1] == 0 {
+            continue;
+        }
+        let dist = get_manattan_distance([0,0], *point);
+        println!("{:?} {}", point, dist);
+        if  dist < min {
+            min = dist; 
+        }
+    }
     return min;
 }
 
-// fn intcode_compute_input(start: &Vec<i32>, noun: i32, verb: i32) -> Vec<i32> {
-//     let mut input = start.clone();
-//     input[1] = noun;
-//     input[2] = verb;
-//     return intcode_compute(&input);
-// }
-
-// fn intcode_compute(start: &Vec<i32>) -> Vec<i32> {
-//     let mut input = start.clone();
-//     let mut i = 0;
-//     loop {
-//         // println!("at input {}",i);
-//         let cmd = input[i];
-//         if cmd == 99 {
-//             // println!("{} means stop", cmd);
-//             break;
-//         }
-        
-//         let op_a = input[input[i+1] as usize];
-//         let op_b = input[input[i+2] as usize];
-//         let output_index = input[i+3] as usize;
-//         let mut result: i32;
-
-//         match cmd {
-//             1 => {
-//                 result = op_a + op_b;
-//             },
-//             2 => {
-//                 result = op_a * op_b;
-//             },
-//             _ => {
-//                 panic!("oh noes, can't do {}", cmd);
-//             }
-//         }
-
-//         input[output_index] = result;
-//         i += 4;
-//     }
-
-//     return input;
-// }
+fn get_manattan_distance(p1: [i32;2], p2: [i32;2]) -> i32 {
+    return (p1[0] - p2[0]).abs() + (p1[1] - p2[1]).abs();
+}
