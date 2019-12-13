518be439b0ef6130b875e5f16c233b11fff137cd
Adam Harvie <adam.harvie@gmail.com>
2019-12-13 13:10:00 -0500

    Day 06 pt 2

--- day_06/main.go
+++ day_06/main.go
@@ -10,14 +10,27 @@ import (
 func main() {
 	tests := []([]([]string)){
 		{
-			[]string{"COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"},
+			[]string{"COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L", "K)YOU", "I)SAN"},
 			[]string{"42"},
 		},
 	}
 
 	for _, test := range tests {
 		fmt.Printf("Test %v\nExpects %v\n", test[0], test[1])
-		fmt.Printf("Actual %v\n", totalOrbits(test[0]))
+		tree := buildTree(test[0])
+		printTree(tree)
+		fmt.Printf("Actual %v\n", treeTotals(tree))
+		// descendant := "I"
+		// anscestor := tree
+		// fmt.Printf("%v is descendant of %v?: %v\n", descendant, anscestor.name, nodeHasDescendant(anscestor, descendant))
+		// fmt.Printf("Path length from %v to %v: %v\n", anscestor.name, descendant, pathLengthToDescendant(anscestor, descendant))
+		origin := getNodeFromTree(tree, "YOU").parent
+		destination := getNodeFromTree(tree, "SAN").parent
+		fmt.Printf("Transfers from %v to %v: %v\n", origin.name, destination.name, getOrbitTransfers(origin, destination.name))
+
+		anscestor2 := "B"
+		descendant2 := tree.children[0].children[0].children[0].children[1]
+		fmt.Printf("Path length of %v to %v is %v\n", descendant2.name, anscestor2, pathLengthToAncestor(descendant2, anscestor2))
 	}
 
 	input, err := getInput()
@@ -26,7 +39,11 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Printf("Day 06 pt 1: %v\n", totalOrbits(input))
+	tree := buildTree(input)
+	fmt.Printf("Day 06 pt 1: %v\n", treeTotals(tree))
+	origin := getNodeFromTree(tree, "YOU").parent
+	destination := getNodeFromTree(tree, "SAN").parent
+	fmt.Printf("Day 06 pt 2: %v\n", getOrbitTransfers(origin, destination.name))
 
 }
 
@@ -57,35 +74,14 @@ type treeNode struct {
 	children []*treeNode
 }
 
-func totalOrbits(orbitMap []string) int {
-	root := buildTree(orbitMap)
-	printTree(root)
-	return treeTotals(root)
-}
-
-// func buildTree(orbitMap []string) treeNode {
-// 	orbits := make(map[string]string)
-// 	// root := treeNode{"COM", nil, make([]*treeNode, 0)}
-// 	treeNodes := make(map[string]*treeNode)
-
-// 	for _, orbitDesc := range orbitMap {
-// 		parts := strings.Split(orbitDesc, ")")
-// 		orbits[parts[1]] = parts[0]
-
-// 	}
-// 	for
-// 	fmt.Println(orbits)
-// 	return treeNode{"", nil, nil}
-// }
-
 func buildTree(orbitMap []string) *treeNode {
-	root := getNode(orbitMap, "COM", nil)
+	root := makeNode(orbitMap, "COM", nil)
 
 	return root
 }
 
-func getNode(orbitMap []string, name string, parent *treeNode) *treeNode {
-	fmt.Printf("getNode %v %v %v\n", len(orbitMap), name, parent)
+func makeNode(orbitMap []string, name string, parent *treeNode) *treeNode {
+	// fmt.Printf("makeNode %v %v %v\n", len(orbitMap), name, parent)
 	node := treeNode{name, parent, make([]*treeNode, 0)}
 	unmatched := make([]string, 0)
 	for _, orbitDesc := range orbitMap {
@@ -99,15 +95,116 @@ func getNode(orbitMap []string, name string, parent *treeNode) *treeNode {
 			unmatched = append(unmatched, orbitDesc)
 		}
 	}
-	fmt.Printf("Umatched %v Children %v\n", len(unmatched), len(node.children))
+
 	if len(unmatched) > 0 && len(node.children) > 0 {
 
 		for i, child := range node.children {
-			node.children[i] = getNode(unmatched, child.name, &node)
+			node.children[i] = makeNode(unmatched, child.name, &node)
 		}
 	}
 
 	return &node
+}
+
+func getOrbitTransfers(origin *treeNode, destination string) int {
+	if origin.name == destination {
+		return 0
+	}
+
+	if nodeHasDescendant(origin, destination) {
+		return pathLengthToDescendant(origin, destination)
+	} else if origin.parent != nil {
+		p := origin.parent
+		count := 0
+		for {
+			if p == nil {
+				break
+			}
+			count++
+			if nodeHasDescendant(p, destination) {
+				return pathLengthToDescendant(p, destination) + count
+			}
+			p = p.parent
+		}
+	}
+
+	return 0
+}
+
+func pathLengthToAncestor(node *treeNode, anscestor string) int {
+	count := 0
+	if node.name == anscestor {
+		return count
+	}
+	if node.parent == nil {
+		return count
+	}
+	p := node.parent
+	for {
+		count++
+		if p.name == anscestor {
+			return count
+		}
+		if p.parent == nil {
+			return 0
+		} else {
+			p = p.parent
+		}
+	}
+}
+func pathLengthToDescendant(node *treeNode, descendant string) int {
+	return pltd(node, descendant, 0) - 1
+}
+func pltd(node *treeNode, descendant string, length int) int {
+	if node.name == descendant {
+		return 1
+	}
+	if len(node.children) == 0 {
+		return 0
+	}
+	for _, child := range node.children {
+		childLength := pltd(child, descendant, length)
+		if childLength > 0 {
+			return childLength + 1
+		}
+	}
+	return 0
+}
+
+func nodeHasDescendant(node *treeNode, search string) bool {
+	if node.name == search {
+		return true
+	}
+
+	if len(node.children) == 0 {
+		return false
+	}
+
+	found := false
+	for _, child := range node.children {
+		found = found || nodeHasDescendant(child, search)
+	}
+
+	return found
+}
+
+func getNodeFromTree(node *treeNode, search string) *treeNode {
+	if node.name == search {
+		return node
+	}
+
+	if len(node.children) == 0 {
+		return nil
+	}
+
+	for _, child := range node.children {
+		found := getNodeFromTree(child, search)
+		if found != nil {
+			return found
+		}
+	}
+
+	return nil
 }
 
 func treeTotals(node *treeNode) int {
