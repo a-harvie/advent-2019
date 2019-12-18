dc88d311ef676f5d057c8c479bfd62bac520f573
Adam Harvie <adam.harvie@gmail.com>
2019-12-18 15:50:19 -0500

    Day 11 pt 2

--- day_11/main.go
+++ day_11/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/a-harvie/advent-2019/input"
 	"github.com/a-harvie/advent-2019/intcode"
@@ -29,8 +30,12 @@ func main() {
 	fmt.Println(program[0])
 
 	r := newRobot(program[0])
-	r.run()
+	r.run(black)
 	fmt.Println(len(r.painted))
+
+	r2 := newRobot(program[0])
+	r2.run(white)
+	render(r2.painted)
 }
 
 func newRobot(program []int) robot {
@@ -46,18 +51,18 @@ func newRobot(program []int) robot {
 	return r
 }
 
-func (r robot) run() {
+func (r robot) run(startColour int) {
 	// Hack to get around race condition - need a better way to signal computer is done :/
 	defer func() {
 		if r := recover(); r != nil {
-			fmt.Println("Guess the robot is done now", r)
+			fmt.Println("Guess the robot is done now ¯\\_(ツ)_/¯:", r)
 		}
 	}()
 	errors := make(chan error)
 
 	// start the program and supply it with the starting camera input
 	go intcode.ChannelCompute(r.program, r.camera, r.output, errors)
-	r.camera <- black
+	r.camera <- startColour
 
 	var outputs [2]int
 	outputIndex := 0
@@ -70,7 +75,6 @@ func (r robot) run() {
 			}
 			outputs[outputIndex] = x
 			outputIndex++
-			// fmt.Printf("Got %v from robot. outputs, index are:%v %v\n", x, outputs, outputIndex)
 		case err, open := <-errors:
 			if !open {
 				fmt.Println("Robot finished - errors closed")
@@ -100,4 +104,49 @@ func (r robot) run() {
 			r.camera <- newColour
 		}
 	}
+}
+
+func render(painted map[position.Coord]int) {
+	minX, maxX, minY, maxY := getBounds(painted)
+
+	// Y is backwards, I guess?
+	for y := maxY; y >= minY; y-- {
+		for x := minX; x <= maxX; x++ {
+			pos := position.Coord{X: x, Y: y}
+			colour, found := painted[pos]
+			if !found {
+				colour = black
+			}
+			if colour == white {
+				fmt.Print("□")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func getBounds(painted map[position.Coord]int) (int, int, int, int) {
+	minX := math.MaxInt64
+	minY := math.MaxInt64
+	maxX := math.MinInt64
+	maxY := math.MinInt64
+
+	for k := range painted {
+		if k.X > maxX {
+			maxX = k.X
+		}
+		if k.X < minX {
+			minX = k.X
+		}
+		if k.Y > maxY {
+			maxY = k.Y
+		}
+		if k.Y < minY {
+			minY = k.Y
+		}
+	}
+
+	return minX, maxX, minY, maxY
 }
